7ac05fcab73a185bd39e7f8bd86d21299155ded0
Firdauz Tri Anggoro <86070626+FirTA@users.noreply.github.com>
2025-02-26 14:29:11 +0700

    add new api

--- src/api/post.js
+++ src/api/post.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const APIWithoutHeader = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://nurse-pro-back-end.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,7 +9,7 @@ export const APIWithoutHeader = axios.create({
 
 
 export const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://nurse-pro-back-end.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
